3ce654b96939a5d347a3a68ef90c490ea42811a3
JesonZHY <503816801@qq.com>
2020-12-03 23:04:50 +0800

    增加注册中心

--- backup/src/main/java/cn/namc/risk/backup/BackupApplication.java
+++ backup/src/main/java/cn/namc/risk/backup/BackupApplication.java
@@ -2,7 +2,9 @@ package cn.namc.risk.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"cn.namc.risk.backup"})
 public class BackupApplication {
 
--- monitor/src/main/java/cn/namc/risk/monitor/MonitorApplication.java
+++ monitor/src/main/java/cn/namc/risk/monitor/MonitorApplication.java
@@ -3,8 +3,10 @@ package cn.namc.risk.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class MonitorApplication {
 
